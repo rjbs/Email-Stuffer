55bf7a39136b44dd36340a755af1c399b35eb362
Ross Attrill <ross.attrill@gmail.com>
2013-06-18 17:36:31 +1000

    #1 Documentation error for Transport method.  Also simplify attaching file in synopsis.

--- lib/Email/Stuffer.pm
+++ lib/Email/Stuffer.pm
@@ -32,8 +32,7 @@ Email::Stuffer - A more casual approach to creating and sending Email:: emails
                 ->to       ('santa@northpole.org'              )
                 ->bcc      ('bunbun@sluggy.com'                )
                 ->text_body($body                              )
-                ->attach   (io('dead_bunbun_faked.gif')->all,
-                            filename => 'dead_bunbun_proof.gif')
+                ->attach_file('dead_bunbun_faked.gif'		   )
                 ->send;
 
 =head1 DESCRIPTION
@@ -474,7 +473,7 @@ C<Email::Sender::Transport::>.  In neither case will a module be
 automatically loaded.
 
 Alternatively, you can pass a complete transport object (which must be
-an L<Email::Send> object) and it will be used as is.
+an L<Email::Sender::Transport> object) and it will be used as is.
 
 =cut
 
