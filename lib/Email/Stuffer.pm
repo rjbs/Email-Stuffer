3f76cd438426084ccc033884bf8fa5b01b0cb3e3
Ricardo Signes <rjbs@cpan.org>
2017-06-24 19:56:22 -0400

    whitespace: drop trailing whitespaces

--- lib/Email/Stuffer.pm
+++ lib/Email/Stuffer.pm
@@ -10,24 +10,24 @@ use Scalar::Util qw(blessed);
   # Prepare the message
   my $body = <<'AMBUSH_READY';
   Dear Santa
-  
+
   I have killed Bun Bun.
-  
+
   Yes, I know what you are thinking... but it was actually a total accident.
-  
+
   I was in a crowded line at a BayWatch signing, and I tripped, and stood on
   his head.
-  
+
   I know. Oops! :/
-  
+
   So anyways, I am willing to sell you the body for $1 million dollars.
-  
+
   Be near the pinhole to the Dimension of Pain at midnight.
-  
+
   Alias
 
   AMBUSH_READY
-  
+
   # Create and send the email in one shot
   Email::Stuffer->from     ('cpan@ali.as'             )
                 ->to       ('santa@northpole.org'     )
@@ -91,7 +91,7 @@ what the correct MIME structure is.
   use Email::MIME;
   use Email::Sender::Simple;
   use IO::All;
-  
+
   Email::Sender::Simple->try_to_send(
     Email::MIME->create(
       header => [
@@ -130,7 +130,7 @@ Email::Stuffer's brevity to your advantage.
 
   package SMS::Alert;
   use base 'Email::Stuffer';
-  
+
   sub new {
     shift()->SUPER::new(@_)
            ->from('monitor@my.website')
@@ -501,7 +501,7 @@ sub attach {
 
 =method attach_file $file [, $header => $value, ... ]
 
-Attachs a file that already exists on the filesystem to the email. 
+Attachs a file that already exists on the filesystem to the email.
 C<attach_file> will auto-detect the MIME type, and use the file's
 current name when attaching.
 
